5d8ad2d4ae39f7349bec4dcd977c0ecc6a0d57b7
DoctorEenot <dreenot@gmail.com>
2024-02-20 22:45:41 +0200

    serialization/deserialization

--- src/models.rs
+++ src/models.rs
@@ -52,9 +52,11 @@ pub struct InvoiceCreateResponse {
     pub invoice_id: Option<String>,
     pub merchant_id: String,
     pub client_id: Option<String>,
+    #[serde(with = "date_format")]
     pub order_id: Option<String>,
-    pub create_date: String,
-    pub modified_date: String,
+    #[serde(with = "date_format")]
+    pub create_date: DateTime<Utc>,
+    pub modified_date: DateTime<Utc>,
     pub status: InvoiceStatus,
     pub pay_url: String,
 }
@@ -68,9 +70,12 @@ pub struct Invoice {
     pub client_id: Option<String>,
     pub status: InvoiceStatus,
     pub status_name: String,
-    pub create_date: String,
-    pub modified_date: String,
-    pub expiration_date: String,
+    #[serde(with = "date_format")]
+    pub create_date: DateTime<Utc>,
+    #[serde(with = "date_format")]
+    pub modified_date: DateTime<Utc>,
+    #[serde(with = "date_format")]
+    pub expiration_date: DateTime<Utc>,
     pub expiration_date_in_milliseconds: u64,
     pub purse: String,
     pub currency: String,
@@ -99,4 +104,42 @@ pub struct Invoice {
     pub failure_url: Option<String>,
     pub success_url: Option<String>,
     pub merchant_site_url: String,
+}
+
+pub mod date_format {
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
+
+    // The signature of a serialize_with function must follow the pattern:
+    //
+    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
+    //    where
+    //        S: Serializer
+    //
+    // although it may also be generic over the input types T.
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    // The signature of a deserialize_with function must follow the pattern:
+    //
+    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
+    //    where
+    //        D: Deserializer<'de>
+    //
+    // although it may also be generic over the output types T.
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let dt = NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)?;
+        Ok(DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc))
+    }
 }
